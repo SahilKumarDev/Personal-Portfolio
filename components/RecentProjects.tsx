4fb277ec7fa78cc0c7460e683f485b22f7d3b6c6
NSxVillan <Sahilkumar710@outlook.com>
2024-05-18 13:50:47 +0530

    Bug fixes

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -5,6 +5,7 @@ import { FaLocationArrow } from "react-icons/fa6";
 import { projects } from "@/data";
 import { PinContainer } from "./ui/Pin";
 import Image from "next/image";
+import Link from "next/link";
 
 const RecentProjects = () => {
   return (
@@ -15,13 +16,14 @@ const RecentProjects = () => {
       </h1>
       <div className="flex flex-wrap items-center md:gap-24 justify-center">
         {projects.map((item) => (
-          <div
+          <Link
+            href={item.link}
             className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw] "
             key={item.id}
           >
             <PinContainer
-              title="/ui.aceternity.com"
-              href="https://twitter.com/mannupaaji"
+              title="sahilkumardev.com"
+              href="https://www.linkedin.com/in/sahil-kumar-dev/"
             >
               <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
                 <div
@@ -74,15 +76,18 @@ const RecentProjects = () => {
                   ))}
                 </div>
 
-                <div className="flex justify-center items-center">
+                <Link
+                  href={item.link}
+                  className="flex justify-center items-center"
+                >
                   <p className="flex lg:text-xl md:text-xs text-sm text-purple">
                     Check Live Site
                   </p>
                   <FaLocationArrow className="ms-3" color="#CBACF9" />
-                </div>
+                </Link>
               </div>
             </PinContainer>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
